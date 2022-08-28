e9e4fd719b151690e1a1c77c0b188fe5cd070c24
Artur <72747079+ArturM998@users.noreply.github.com>
2022-08-28 16:52:58 +0200

    added logging in to O2 mailbox

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def deleteO2Mails():
+    driver = webdriver.Chrome()
+    driver.get("https://poczta.o2.pl/login/login.html")
+    driver.find_element("xpath", "//button[text()='AKCEPTUJĘ I PRZECHODZĘ DO SERWISU']").click()
+    driver.find_element("id", "login").send_keys(input("Email address: "))
+    driver.find_element("id", "password").send_keys(input("Password: ") + Keys.ENTER)
 
+    time.sleep(5)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    deleteO2Mails()
