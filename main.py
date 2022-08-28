19ffef76c01fd655e36e335a05eb39e91254ed9c
Artur <72747079+ArturM998@users.noreply.github.com>
2022-08-28 17:31:45 +0200

    added deleting all messages from O2 mailbox

--- main.py
+++ main.py
@@ -3,14 +3,27 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
+
 def deleteO2Mails():
     driver = webdriver.Chrome()
     driver.get("https://poczta.o2.pl/login/login.html")
     driver.find_element("xpath", "//button[text()='AKCEPTUJĘ I PRZECHODZĘ DO SERWISU']").click()
     driver.find_element("id", "login").send_keys(input("Email address: "))
     driver.find_element("id", "password").send_keys(input("Password: ") + Keys.ENTER)
+    driver.find_elements()
 
-    time.sleep(5)
+    while len(driver.find_elements("xpath", "//button[text()='Folder Odebrane jest pusty']")) == 0:
+        driver.find_element("xpath", "//*[contains(text(), 'Zaznacz wszystkie')]").click()
+        time.sleep(0.5)
+        driver.find_element("name", "delete-20px").click()
+        time.sleep(0.5)
+        if len(driver.find_elements("xpath", "//*[contains(text(), 'Tak, przenoszę wszystkie wiadomości')]")) != 0:
+            driver.find_element("xpath", "//*[contains(text(), 'Tak, przenoszę wszystkie wiadomości')]").click()
+        time.sleep(1.5)
+
+    while True:
+        time.sleep(1)
+
 
 if __name__ == '__main__':
     deleteO2Mails()
