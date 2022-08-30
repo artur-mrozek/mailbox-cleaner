a875f3c5b9d4fb005a8fc8ac57f05fb52c1109a5
Artur <72747079+ArturM998@users.noreply.github.com>
2022-08-30 15:19:11 +0200

    added wp support

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.chrome.options import Options
 
 
 def deleteO2Mails():
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
     driver.get("https://poczta.o2.pl/login/login.html")
+    time.sleep(1)  # wait for cookie prompt
     driver.find_element("xpath", "//button[text()='AKCEPTUJĘ I PRZECHODZĘ DO SERWISU']").click()
     driver.find_element("id", "login").send_keys(input("Email address: "))
     driver.find_element("id", "password").send_keys(input("Password: ") + Keys.ENTER)
-    driver.find_elements()
+    time.sleep(3)  # wait for log in
 
     while len(driver.find_elements("xpath", "//button[text()='Folder Odebrane jest pusty']")) == 0:
         driver.find_element("xpath", "//*[contains(text(), 'Zaznacz wszystkie')]").click()
@@ -20,10 +23,39 @@ def deleteO2Mails():
         if len(driver.find_elements("xpath", "//*[contains(text(), 'Tak, przenoszę wszystkie wiadomości')]")) != 0:
             driver.find_element("xpath", "//*[contains(text(), 'Tak, przenoszę wszystkie wiadomości')]").click()
         time.sleep(1.5)
+    print("done")
 
-    while True:
+
+def deleteWPMails():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://profil.wp.pl/login/login.html")
+    time.sleep(1)  # wait for cookie prompt
+    driver.find_element("xpath", "//button[text()='AKCEPTUJĘ I PRZECHODZĘ DO SERWISU']").click()
+    driver.find_element("id", "login").send_keys(input("Email address: "))
+    driver.find_element("id", "password").send_keys(input("Password: ") + Keys.ENTER)
+    time.sleep(3)  # wait for log in
+
+    while len(driver.find_elements("xpath", "//button[text()='Folder Odebrane jest pusty']")) == 0:
+        driver.find_element("xpath", "//nh-checkbox[@aria-label='Zaznacz wiadomości']").click()
         time.sleep(1)
+        driver.find_element("xpath", "//*[contains(text(), 'usuń')]").click()
+        time.sleep(1)
+        if len(driver.find_elements("xpath", "//button[@data-sel-elem='modal_empty_trash_accept']")) != 0:
+            driver.find_element("xpath", "//button[@data-sel-elem='modal_empty_trash_accept']").click()
+        time.sleep(2)
+    print("done")
 
 
 if __name__ == '__main__':
-    deleteO2Mails()
+    while True:
+        option = input("Type\no2 - clear o2 mailbox\nwp - clear wp mailbox\nexit - exit programm\n")
+        if option == "wp":
+            deleteWPMails()
+        elif option == "o2":
+            deleteO2Mails()
+        elif option == "exit":
+            break
+        else:
+            print("Type correct input")
